9aaf105abd5a41e6d3fdfdb040c3ae2d0b915ec2
vaibhav-saxena25 <149486002+vaibhav-saxena25@users.noreply.github.com>
2024-08-07 19:45:22 +0530

    Update server.js
    chnage 1

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname,'../client/build')));
+app.use(express.static(path.join(__dirname, 'client/build/build')));
 
 app.use('/api/v1/portfolio',require('./routes/portfolioRoutes'));
 
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../client/build/index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/build/index.html'));
 });
 
 app.get('/',(req,res)=>{
