946a6f2a369c9b3cb0d497a2f5b6b19a87a9b4de
vaibhav-saxena25 <149486002+vaibhav-saxena25@users.noreply.github.com>
2024-08-07 19:08:49 +0530

    Production app
    this is my production app

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname,'../client/build')));
+
+app.use('/api/v1/portfolio',require('./routes/portfolioRoutes'));
+
+app.get('*',function(req,res)=>{
+    res.sendFile(path.join(__dirname,'../client/build/index.html'));
+});
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Welcome to Node Server</h1>');
+})
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
